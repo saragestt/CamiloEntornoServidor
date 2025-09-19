0337511657677c2bab84ad7539a10ebf4e9fa2c0
saragestt <iamsaramusic@gmail.com>
2025-09-19 14:08:23 +0200

    Día 2

--- AprendiendoPHP/ejercicios.php
+++ AprendiendoPHP/ejercicios.php
@@ -1,5 +1,5 @@
 <?php
-
+//Bucles 
 /*array CLAVE-VALOR que se llame productos. Este elemento tendrá dentro arrays. Cada array interno tendrá una
 estructura id, name, price, stock, category. Que sean 5 elementos. Luego crear otro array llamado carrito.
 Este array tendrá otros arrays internos. Cada array interno tendrá lo siguiente:
@@ -72,9 +72,49 @@ foreach($productos as $producto){
    }
 }
 echo " Precio total: " . $cont;
+echo "<br>";
 
+/* Tenemos un array llamado $users o $ usuarios. Este array es
+ bidimensional. La estructura de los arrays internos tienen 
+ username, password, role (ADMIN O USER), isActive(true o false).
 
+ Cuando pongais el isActive ponedlo como json_encode (true o false).
 
+ Cread una variable array, que tendra username, y un password. 
 
+ - Comprobar si el usuario existe en numestra bbdd($users) Si no
+   existe al final de toda la comprobación habra que poner un 
+   mensaje que diga "Usuario no existe".
+- Si existe, y la contraseña es incorrecta, poned "Contraseña 
+  incorrecta".
+- Si todo coincide, poned "Bienvenido [USERNAME] Rol[ROL]". 
+- Luego crear un array unidimensional, cuya posicion 0 sea 
+  "Acceso total", la 
+  posicion 1 será "Acceso limitado". */
 
+  echo "<br>";
 
+  $users = array(
+    ["username" => "lolo27", "password" => "Lol123", "rol" => "user", "isActive" => json_encode(true)],
+    ["username" => "bottle30", "password" => "ADM456", "rol" => "admin", "isActive" => json_encode(false)],
+    ["username" => "lolIPOP15", "password" => "pop789", "rol" => "user", "isActive" => json_encode(true)],
+  );
+
+foreach ($users as $usuario){
+  print_r($usuario);
+  echo"<br>";
+};
+
+  echo"<br>";
+
+foreach ($users as $usuario){
+    if(in_array("username",$users)){
+        if ($users["username"] == "lolo27"){
+            echo "El usuario existe"; 
+        }
+        else{
+            echo "Contraseña incorrecta";
+        }
+    }
+};
+ 
